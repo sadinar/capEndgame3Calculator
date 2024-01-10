a63602c6f25b882edd37a610885f94af03bc66db
Sadinar <sadinar@gmail.com>
2024-01-09 22:43:32 -0600

    improved unit tests

--- calculators/giant_chance_test.go
+++ calculators/giant_chance_test.go
@@ -93,58 +93,69 @@ func TestFindNextUpgrade(t *testing.T) {
 	assert.Equal(t, 5, sl.findNextUpgrade())
 }
 
+func TestGetNextUpgrade(t *testing.T) {
+	gc := NewGiantCalculator(
+		NewOverclockConfig(false, false, false, false, false),
+		1.0,
+		1.0,
+		0.5,
+		map[int]int{
+			2: 0,
+			3: 0,
+			4: 0,
+			5: 0,
+		},
+		0,
+	)
+	nu := gc.GetNextUpgrade()
+	assert.Equal(t, "upgrade x2 strike", nu)
+
+	gc.strikeUpgrades[DoubleStrike] = 1
+	nu = gc.GetNextUpgrade()
+	assert.Equal(t, "upgrade x3 strike", nu)
+
+	gc.strikeUpgrades[TripleStrike] = 1
+	nu = gc.GetNextUpgrade()
+	assert.Equal(t, "upgrade x4 strike", nu)
+
+	gc.strikeUpgrades[QuadrupleStrike] = 1
+	nu = gc.GetNextUpgrade()
+	assert.Equal(t, "upgrade x5 strike", nu)
+
+	gc.strikeUpgrades[QuintupleStrike] = 1
+	nu = gc.GetNextUpgrade()
+	assert.Equal(t, "giant luck", nu)
+
+	gc.strikeUpgrades[DoubleStrike] = 70
+	gc.strikeUpgrades[TripleStrike] = 70
+	gc.strikeUpgrades[QuadrupleStrike] = 70
+	gc.strikeUpgrades[QuintupleStrike] = 70
+	nu = gc.GetNextUpgrade()
+	assert.Equal(t, "giant luck", nu)
+
+	gc.giantLuckUpgrade = 59
+	nu = gc.GetNextUpgrade()
+	assert.Equal(t, "giant luck", nu)
+
+	gc.giantLuckUpgrade = 60
+	nu = gc.GetNextUpgrade()
+	assert.Equal(t, "upgrade x2 strike", nu)
+}
+
 func getCalculatorWithDummyPrices() GiantCalculator {
 	return GiantCalculator{
 		strikeUpgrades: map[int]int{},
-		strikePrices: map[int]upgradeCostList{
-			DoubleStrike: {
-				1:  20000,
-				2:  40000,
-				3:  60000,
-				4:  80000,
-				5:  100000,
-				6:  120000,
-				7:  140000,
-				8:  160000,
-				9:  180000,
-				10: 200000,
-			},
-			TripleStrike: {
-				1:  30000,
-				2:  60000,
-				3:  90000,
-				4:  120000,
-				5:  150000,
-				6:  180000,
-				7:  210000,
-				8:  240000,
-				9:  270000,
-				10: 300000,
-			},
-			QuadrupleStrike: {
-				1:  40000,
-				2:  80000,
-				3:  120000,
-				4:  160000,
-				5:  200000,
-				6:  240000,
-				7:  280000,
-				8:  320000,
-				9:  360000,
-				10: 400000,
-			},
-			QuintupleStrike: {
-				1:  50000,
-				2:  100000,
-				3:  150000,
-				4:  200000,
-				5:  250000,
-				6:  300000,
-				7:  350000,
-				8:  400000,
-				9:  450000,
-				10: 500000,
-			},
+		strikePrices: upgradeCostList{
+			1:  20000,
+			2:  40000,
+			3:  60000,
+			4:  80000,
+			5:  100000,
+			6:  120000,
+			7:  140000,
+			8:  160000,
+			9:  180000,
+			10: 200000,
 		},
 		giantLuckPrices: map[int]int{
 			1:  100000,
